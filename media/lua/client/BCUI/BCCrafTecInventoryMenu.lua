fc3bb7ca53fbcb70a60b4faa3b58a3445b8257f6
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2015-06-18 12:38:18 +0200

    add tooltip

--- media/lua/client/BCUI/BCCrafTecInventoryMenu.lua
+++ media/lua/client/BCUI/BCCrafTecInventoryMenu.lua
@@ -1,6 +1,6 @@
 BCCrafTecInventoryMenu = {};
 
-BCCrafTecInventoryMenu.StartCrafTec = function (player, item, requirements)
+BCCrafTecInventoryMenu.StartCrafTec = function (player, item, requirements) -- {{{
 	player = getSpecificPlayer(player);
 	local CrafTecItem = player:getInventory():AddItem("CrafTec.Project");
 	local modData = CrafTecItem:getModData();
@@ -11,13 +11,13 @@ BCCrafTecInventoryMenu.StartCrafTec = function (player, item, requirements)
 
 	ISTimedActionQueue.add(BCCrafTec:new(player, CrafTecItem, 0));
 end
-
-BCCrafTecInventoryMenu.ContinueCrafTec = function(player, item)
+-- }}}
+BCCrafTecInventoryMenu.ContinueCrafTec = function(player, item) -- {{{
 	player = getSpecificPlayer(player);
 	ISTimedActionQueue.add(BCCrafTec:new(player, item, 0));
 end
-
-BCCrafTecInventoryMenu.InventoryMenu = function(player, context, items)
+-- }}}
+BCCrafTecInventoryMenu.InventoryMenu = function(player, context, items) -- {{{
 	item = items[1];
 	if not instanceof(item, "InventoryItem") then
 		item = item.items[1];
@@ -34,6 +34,81 @@ BCCrafTecInventoryMenu.InventoryMenu = function(player, context, items)
 
 	local requirements = { any = { any = { level = 0, time = 60, progress = 0 } } };
 	subMenu:addOption("Craft Stone Axe", player, BCCrafTecInventoryMenu.StartCrafTec, "Base.AxeStone", requirements);
+
+	local requirements = {
+		Engineer = {
+			Woodwork = {
+				level = 2,
+				time = 600,
+				progress = 0
+			}
+		},
+		Electrician = {
+			Electricity = {
+				level = 5,
+				time = 300,
+				progress = 0
+			}
+		},
+		any = {
+			any = {
+				level = 0,
+				time = 120,
+				progress = 0
+			}
+		}
+	};
+	subMenu:addOption("Craft a generator", player, BCCrafTecInventoryMenu.StartCrafTec, "Base.Generator", requirements);
 end
+-- }}}
+
+BCCrafTecInventoryMenu.ISToolTipInvRender = ISToolTipInv.render;
+function ISToolTipInv:render() -- {{{
+	BCCrafTecInventoryMenu.ISToolTipInvRender(self);
+	if self.item:getFullType() ~= "CrafTec.Project" then return end;
+
+	local th = self.height;
+	local r = 0;
+	local g = 0;
+
+	local text = {};
+
+	local modData = self.item:getModData()["CrafTec"];
+	table.insert(text, "Project: "..modData["product"]);
+
+	for k,profession in pairs(modData["requirements"]) do
+		table.insert(text, "Profession: "..k);
+		for k,skill in pairs(profession) do
+			if k ~= "any" then
+				table.insert(text, "  Skill: "..k.." Level "..skill["level"]);
+			end
+			table.insert(text, "    Progress: "..skill["progress"].." / "..skill["time"]);
+		end
+	end
+
+	local y = th;
+	local w = self.width;
+
+	for _,t in ipairs(text) do
+		local textHeight = getTextManager():MeasureStringY(UIFont.Small, t);
+		local textWidth = getTextManager():MeasureStringX(UIFont.Small, t);
+		-- self:drawText(t, 3, y+3, 1.0, 1.0, 0.8, 1, UIFont.Small);
+		y = y + textHeight + 3;
+		w = math.max(w, textWidth);
+	end
+
+	self:drawRect(0, th, math.max(self.width, 6+w), 6+y-th,
+		self.backgroundColor.a, self.backgroundColor.r, self.backgroundColor.g, self.backgroundColor.b);
+	self:drawRectBorder(0, th, math.max(self.width, 6+w), 6+y-th,
+		self.borderColor.a, self.borderColor.r, self.borderColor.g, self.borderColor.b);
+
+	y = th;
+	for _,t in ipairs(text) do
+		local textHeight = getTextManager():MeasureStringY(UIFont.Small, t);
+		self:drawText(t, 3, y+3, 1.0, 1.0, 0.8, 1, UIFont.Small);
+		y = y + textHeight + 3;
+	end
+end
+-- }}}
 
 Events.OnFillInventoryObjectContextMenu.Add(BCCrafTecInventoryMenu.InventoryMenu);
