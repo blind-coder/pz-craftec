4e9193ff4d177ca157faf1b7c72027e3f6fe136c
Benjamin 'blindCoder' Schieder <benjamin@anderdonau.de>
2016-03-28 13:38:36 +0200

    some backwards compat

--- media/lua/shared/BCCrafTec.lua
+++ media/lua/shared/BCCrafTec.lua
@@ -4,6 +4,9 @@ BCCrafTec.getImages = function(player, recipe)
 	if recipe.images.any ~= nil then
 		return recipe.images.any;
 	end
+	if recipe.images.west ~= nil then -- backwards compat
+		return recipe.images;
+	end
 	for perk,levels in pairs(recipe.images) do
 		local retVal = {};
 		local perkLevel = player:getPerkLevel(Perks.FromString(perk));
